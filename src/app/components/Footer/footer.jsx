cf80b3d37be4c3620e43dfb89c823df8f7bef119
rtrivaldo <rtrivaldo05@gmail.com>
2024-08-07 11:34:51 +0700

    add target blank on footer links

--- src/app/components/Footer/footer.jsx
+++ src/app/components/Footer/footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
             <p>Design & Developed by Rivaldo Tandoko.</p>
             <p>
                 Data provided by{" "}
-                <Link href={"https://www.themoviedb.org/"} className="underline">
+                <Link href={"https://www.themoviedb.org/"} target="_blank" className="underline">
                     TMDb
                 </Link>
                 .
@@ -15,14 +15,14 @@ function Footer() {
             {/* socials */}
             <div className="mt-4 flex items-center gap-6 text-2xl lg:text-3xl">
                 {/* tiktok */}
-                <Link href={"https://www.tiktok.com/@sycle.dev?_t=8odqA9nwTSh&_r=1"}>
+                <Link href={"https://www.tiktok.com/@sycle.dev?_t=8odqA9nwTSh&_r=1"} target="_blank">
                     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
                         <path fill="currentColor" d="M16 8.245V15.5a6.5 6.5 0 1 1-5-6.326v3.163a3.5 3.5 0 1 0 2 3.163V2h3a5 5 0 0 0 5 5v3a7.97 7.97 0 0 1-5-1.755" />
                     </svg>
                 </Link>
 
                 {/* github */}
-                <Link href={"https://github.com/rtrivaldo"}>
+                <Link href={"https://github.com/rtrivaldo"} target="_blank">
                     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
                         <path
                             fill="currentColor"
@@ -32,14 +32,14 @@ function Footer() {
                 </Link>
 
                 {/* linkedin */}
-                <Link href={"https://www.linkedin.com/in/rivaldo-tandoko/"}>
+                <Link href={"https://www.linkedin.com/in/rivaldo-tandoko/"} target="_blank">
                     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
                         <path fill="currentColor" d="M18.336 18.339h-2.665v-4.177c0-.996-.02-2.278-1.39-2.278c-1.389 0-1.601 1.084-1.601 2.205v4.25h-2.666V9.75h2.56v1.17h.035c.358-.674 1.228-1.387 2.528-1.387c2.7 0 3.2 1.778 3.2 4.092v4.714M7.004 8.575a1.546 1.546 0 0 1-1.548-1.549a1.548 1.548 0 1 1 1.547 1.549m1.336 9.764H5.667V9.75H8.34zM19.67 3H4.33C3.594 3 3 3.58 3 4.297v15.406C3 20.42 3.594 21 4.328 21h15.339C20.4 21 21 20.42 21 19.703V4.297C21 3.581 20.4 3 19.666 3z" />
                     </svg>
                 </Link>
 
                 {/* email */}
-                <Link href={"mailto:rtrivaldo05@gmail.com"}>
+                <Link href={"mailto:rtrivaldo05@gmail.com"} target="_blank">
                     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
                         <path fill="currentColor" d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2m-.4 4.25l-7.07 4.42c-.32.2-.74.2-1.06 0L4.4 8.25a.85.85 0 1 1 .9-1.44L12 11l6.7-4.19a.85.85 0 1 1 .9 1.44" />
                     </svg>
