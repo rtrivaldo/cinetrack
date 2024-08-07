dfd0325659f1c105247ea48b0e3d4859be0babbf
rtrivaldo <rtrivaldo05@gmail.com>
2024-08-07 11:35:33 +0700

    fix bugs on footer links

--- src/app/components/Footer/footer.jsx
+++ src/app/components/Footer/footer.jsx
@@ -39,7 +39,7 @@ function Footer() {
                 </Link>
 
                 {/* email */}
-                <Link href={"mailto:rtrivaldo05@gmail.com"} target="_blank">
+                <Link href={"mailto:rtrivaldo05@gmail.com"}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
                         <path fill="currentColor" d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2m-.4 4.25l-7.07 4.42c-.32.2-.74.2-1.06 0L4.4 8.25a.85.85 0 1 1 .9-1.44L12 11l6.7-4.19a.85.85 0 1 1 .9 1.44" />
                     </svg>
