45d485664e9253418d4e77aaed89b587ae4332c7
rtrivaldo <rtrivaldo05@gmail.com>
2024-08-09 14:29:32 +0700

    add aos on navbar

--- src/app/components/Navbar/navbar.js
+++ src/app/components/Navbar/navbar.js
@@ -19,7 +19,7 @@ function Navbar() {
 
     return (
         <>
-            <div className="p-4 md:p-6 lg:hidden fixed top-0 z-50 w-screen bg-background">
+            <div className="p-4 md:p-6 lg:hidden fixed top-0 z-50 w-screen bg-background" data-aos="fade">
                 <input
                     type="text"
                     name="search"
